21d4544577cc1339a772b35536a4f72519b37100
Zhaowei Zhou <zhouzhaowei0@gmail.com>
2019-02-18 19:15:17 -0600

    last version

--- pasword.py
+++ pasword.py
@@ -1,10 +1,15 @@
 password = 'a123456'
 i = 3 #剩余机会
 while i > 0:
+	i = i - 1
 	pwd = input('请输入密码')
 	if pwd == password:
 		print('欢迎登陆')
 		break #逃出回圈
 	else:
-		i = i - 1
-		print('密码错误还有', i, '次机会')
+		print('密码错误!')
+		if i > 0:
+			print('还有', i, '次机会')
+		else:
+			print('账户已锁定！')
+
